8caca3f8e2eecd6fefa4def5557cca3615ee7c02
Kevin (Pololu) <dev-kevin@pololu.com>
2019-05-31 15:57:07 -0700

    major cleanup and rewrite
    renamed to HighPowerStepperDriver; removed functions for more advanced
    settings; added friendly current limit setting function; added Doxygen
    stuff

---
+++ HighPowerStepperDriver.h
@@ -0,0 +1,495 @@
+// Copyright Pololu Corporation.  For more information, see http://www.pololu.com/
+
+/// \file DRV8711.h
+///
+/// This is the main header file for the DRV8711 library, a library for
+/// controlling the DRV8711 micro-stepping stepper motor driver.
+///
+/// For more information about this library, see:
+///
+///   https://github.com/pololu/drv8711-arduino
+///
+/// That is the main repository for this library.
+
+#pragma once
+
+#include <Arduino.h>
+#include <SPI.h>
+
+/// This class provides low-level functions for reading and writing from the SPI
+/// interface of a DRV8711 stepper motor controller IC.
+///
+/// Most users should use the HighPowerStepperDriver class, which provides a
+/// higher-level interface, instead of this class.
+class DRV8711SPI
+{
+public:
+  /// Configures this object to use the specified pin as a chip select pin.
+  ///
+  /// You must use a chip select pin; the DRV8711 requires it.
+  void setChipSelectPin(uint8_t pin)
+  {
+    csPin = pin;
+    digitalWrite(csPin, LOW);
+    pinMode(csPin, OUTPUT);
+  }
+
+  /// Reads the register at the given address and returns its raw value.
+  uint16_t readReg(uint8_t address)
+  {
+    // Read/write bit and register address are the first 4 bits of the first
+    // byte; data is in the remaining 4 bits of the first byte combined with
+    // the second byte (12 bits total).
+
+    selectChip();
+    uint16_t dataOut = transfer((0x8 | (address & 0b111)) << 12);
+    deselectChip();
+    return dataOut & 0xFFF; // Mask off read/write bit
+  }
+
+  /// Writes the specified value to a register.
+  void writeReg(uint8_t address, uint16_t value)
+  {
+    // Read/write bit and register address are the first 4 bits of the first
+    // byte; data is in the remaining 4 bits of the first byte combined with
+    // the second byte (12 bits total).
+
+    selectChip();
+    transfer(((address & 0b111) << 12) | (value & 0xFFF));
+
+    // The CS line must go low after writing for the value to actually take
+    // effect.
+    deselectChip();
+  }
+
+private:
+
+  SPISettings settings = SPISettings(500000, MSBFIRST, SPI_MODE0);
+
+  uint16_t transfer(uint16_t value)
+  {
+    return SPI.transfer16(value);
+  }
+
+  void selectChip()
+  {
+    digitalWrite(csPin, HIGH);
+    SPI.beginTransaction(settings);
+  }
+
+  void deselectChip()
+  {
+   SPI.endTransaction();
+   digitalWrite(csPin, LOW);
+  }
+
+  uint8_t csPin;
+};
+
+
+  /// Addresses of control and status registers.
+  enum HPSDRegAddr : uint8_t
+  {
+    CTRL   = 0x00,
+    TORQUE = 0x01,
+    OFF    = 0x02,
+    BLANK  = 0x03,
+    DECAY  = 0x04,
+    STALL  = 0x05,
+    DRIVE  = 0x06,
+    STATUS = 0x07,
+  };
+
+  /// Possible arguments to setStepMode().
+  enum HPSDStepMode : uint16_t
+  {
+    MicroStep256 = 256,
+    MicroStep128 = 128,
+    MicroStep64  =  64,
+    MicroStep32  =  32,
+    MicroStep16  =  16,
+    MicroStep8   =   8,
+    MicroStep4   =   4,
+    MicroStep2   =   2,
+    MicroStep1   =   1,
+  };
+
+  /// Possible arguments to setDecayMode().
+  enum class HPSDDecayMode : uint8_t
+  {
+    Slow                = 0b000,
+    SlowIncMixedDec     = 0b001,
+    Fast                = 0b010,
+    Mixed               = 0b011,
+    SlowIncAutoMixedDec = 0b100,
+    AutoMixed           = 0b101,
+  };
+
+  /// Bitmasks for the return value of readStatus().
+  ///
+  /// See the DRV8711 datasheet for detailed descriptions of these status
+  /// conditions.
+  enum class HPSDStatusBit : uint8_t
+  {
+    /// Overtemperature shutdown
+    OTS = (1 << 0), 
+    
+    /// Channel A overcurrent shutdown
+    AOCP = (1 << 1),
+    
+    /// Channel B overcurrent shutdown
+    BOCP = (1 << 2),
+    
+    /// Channel A predriver fault
+    APDF = (1 << 3),
+    
+    /// Channel B predriver fault
+    BPDF = (1 << 4),
+    
+    /// Undervoltage lockout
+    UVLO = (1 << 5),
+    
+    /// Stall detected
+    STD = (1 << 6),
+    
+    /// Latched stall detect
+    STDLAT = (1 << 7),
+  };
+
+
+/// This class provides high-level functions for controlling a DRV8711-based 
+/// High-Power Stepper Motor Driver.
+class HighPowerStepperDriver
+{
+public:  
+  /// The default constructor.
+  HighPowerStepperDriver()
+  {
+    // All settings set to power-on defaults
+    ctrl   = 0xC10;
+    torque = 0x1FF;
+    off    = 0x030;
+    blank  = 0x080;
+    decay  = 0x110;
+    stall  = 0x040;
+    drive  = 0xA59;
+  }
+
+
+  /// Configures this object to use the specified pin as a chip select pin.
+  /// You must use a chip select pin; the DRV8711 requires it.
+  void setChipSelectPin(uint8_t pin)
+  {
+    driver.setChipSelectPin(pin);
+  }
+
+  /// Changes all of the driver's settings back to their default values.
+  ///
+  /// It is good to call this near the beginning of your program to ensure that
+  /// there are no settings left over from an earlier time that might affect the
+  /// operation of the driver.
+  void resetSettings()
+  {
+    ctrl   = 0xC10;
+    torque = 0x1FF;
+    off    = 0x030;
+    blank  = 0x080;
+    decay  = 0x110;
+    stall  = 0x040;
+    drive  = 0xA59;
+    applySettings();
+    clearStatus();
+  }
+
+  /// Reads back the SPI configuration registers from the device and verifies
+  /// that they are equal to the cached copies stored in this class.
+  ///
+  /// This can be used to verify that the driver is powered on and has not lost
+  /// them due to a power failure.  The STATUS register is not verified because
+  /// it does not contain any driver settings.
+  ///
+  /// @return 1 if the settings from the device match the cached copies, 0 if
+  /// they do not.
+  bool verifySettings()
+  {
+    return driver.readReg(CTRL)   == ctrl   &&
+           driver.readReg(TORQUE) == torque &&
+           driver.readReg(OFF)    == off    &&
+           driver.readReg(BLANK)  == blank  &&
+           driver.readReg(DECAY)  == decay  &&
+           driver.readReg(STALL)  == stall  &&
+           driver.readReg(DRIVE)  == drive;
+  }
+  
+  /// Re-writes the cached settings stored in this class to the device.
+  ///
+  /// You should not normally need to call this function because settings are
+  /// written to the device whenever they are changed.  However, if
+  /// verifySettings() returns false (due to a power interruption, for
+  /// instance), then you could use applySettings to get the device's settings
+  /// back into the desired state.
+  void applySettings()
+  {
+    writeTORQUE();
+    writeOFF();
+    writeBLANK();
+    writeDECAY();
+    writeDRIVE();
+    writeSTALL();
+    
+    // CTRL is written last because it contains the ENBL bit, and we want to try
+    // to have all the other settings correct first.  (For example, TORQUE
+    // defaults to 0xFF (the maximum value), so it would be better to set a more
+    // appropriate value if necessary before enabling the motor.)
+    writeCTRL();
+  }
+
+  /// Enables the driver (ENBL = 1).
+  void enableDriver()
+  {
+    ctrl |= (1 << 0);
+    writeCTRL();
+  }
+
+  /// Disables the driver (ENBL = 0).
+  void disableDriver()
+  {
+    ctrl &= ~(1 << 0);
+    writeCTRL();
+  }
+
+  /// Sets the motor direction (RDIR).
+  ///
+  /// Allowed values are 0 or 1.
+  ///
+  /// You can use this command to control the direction of the stepper motor and
+  /// simply leave the DIR pin disconnected.
+  void setDirection(bool value)
+  {
+    if (value)
+    {
+      ctrl |= (1 << 1);
+    }
+    else
+    {
+      ctrl &= ~(1 << 1);
+    }
+    writeCTRL();
+  }
+
+  /// Returns the cached value of the motor direction (RDIR).
+  ///
+  /// This does not perform any SPI communication with the driver.
+  bool getDirection()
+  {
+    return ctrl >> 1 & 1;
+  }
+
+  /// Advances the indexer by one step (RSTEP = 1).
+  ///
+  /// The driver automatically clears the RSTEP bit after it is written.
+  void step()
+  {
+    ctrl |= (1 << 2);
+    writeCTRL();
+  }
+
+  /// Sets the driver's stepping mode (MODE).
+  ///
+  /// This affects many things about the performance of the motor, including how
+  /// much the output moves for each step taken and how much current flows
+  /// through the coils in each stepping position.
+  ///
+  /// The argument to this function should be one of the members of the
+  /// #StepMode enum.
+  ///
+  /// If an invalid stepping mode is passed to this function, then it selects
+  /// 1/4 micro-step, which is the driver's default.
+  void setStepMode(uint16_t mode)
+  {
+    // Pick 1/4 micro-step by default.
+    uint8_t sm = 0b0010;
+
+    switch (mode)
+    {
+    case MicroStep1:   sm = 0b0000; break;
+    case MicroStep2:   sm = 0b0001; break;
+    case MicroStep4:   sm = 0b0010; break;
+    case MicroStep8:   sm = 0b0011; break;
+    case MicroStep16:  sm = 0b0100; break;
+    case MicroStep32:  sm = 0b0101; break;
+    case MicroStep64:  sm = 0b0110; break;
+    case MicroStep128: sm = 0b0111; break;
+    case MicroStep256: sm = 0b1000; break;
+    }
+
+    ctrl = (ctrl & 0b111110000111) | (sm << 3);
+    writeCTRL();
+  }
+
+  /// Sets the current limit for a High-Power Stepper Motor Driver 36v40.
+  ///
+  /// The argument to this function should be the desired current limit in
+  /// milliamps.
+  ///
+  /// WARNING: The 36v40 can supply up to about 4 A per coil continuously;
+  /// higher currents might be sustainable for short periods, but can eventually
+  /// cause the MOSFETs to overheat, which could damage them.  See the driver's
+  /// product page for more information.
+  ///
+  /// This function allows you to set a current limit of up to 8 A (8000 mA),
+  /// but we strongly recommend against using a current limit higher than 4 A
+  /// (4000 mA) unless you are careful to monitor the MOSFETs' temperatures
+  /// and/or restrict how long the driver uses the higher current limit.
+  ///
+  /// This function takes care of setting appropriate values for ISGAIN and
+  /// TORQUE to get the desired current limit.
+  void setCurrentLimit36v40(uint16_t limit)
+  {
+    // From the DRV8711 datasheet, section 7.3.4, equation 2:
+    //
+    //   Ifs = (2.75 V * TORQUE) / (256 * ISGAIN * Risense)
+    //
+    // Rearranged:
+    //
+    //   TORQUE = (256 * ISGAIN * Risense * Ifs) / 2.75 V
+    //
+    // The 36v40 has an Risense of 30 milliohms, and "limit" is in milliamps, so
+    // we divide it by 1000 to convert to Ifs in amps:
+    //
+    //   TORQUE = (256 * ISGAIN * 0.03 ohms * (limit/1000) A) / 2.75 V
+    //          = (0.00768 * ISGAIN * limit) / 2.75
+    //
+    // We want to pick the highest gain (5, 10, 20, or 40) that will not
+    // overflow TORQUE (8 bits, 0xFF max). 
+
+    if (limit > 8000) { limit = 8000; }
+    
+    // Start with a gain of 40 and calculate the TORQUE value needed.
+    uint8_t isgainBits = 0b11;   
+    uint16_t torqueBits = ((uint32_t)768  * limit) / 6875;
+    
+    // Halve the gain and TORQUE until the TORQUE value fits in 8 bits.
+    while (torqueBits > 0xFF)
+    {
+      isgainBits--;
+      torqueBits >>= 1;
+    }
+
+    ctrl = (ctrl & 0b110011111111) | (isgainBits << 8);
+    writeCTRL();
+    torque = (torque & 0b111100000000) | torqueBits;
+    writeTORQUE();
+  }
+
+  /// Sets the driver's decay mode (DECMOD).
+  ///
+  /// The argument to this function should be one of the members of the
+  /// #DecayMode enum.
+  void setDecayMode(HPSDDecayMode mode)
+  {
+    decay = (decay & 0b00011111111) | (((uint8_t)mode & 0b111) << 8);
+    writeDECAY();
+  }
+
+  /// Reads the status of the driver (STATUS register).
+  ///
+  /// The return value is an 8-bit unsigned integer that has one bit for each
+  /// status condition (the upper 4 bits of the 12-bit STATUS register are not
+  /// used).  You can simply compare the return value to 0 to see if any of the
+  /// status bits are set, or you can use the logical AND operator (`&`) and the
+  /// #StatusBit enum to check individual bits.
+  uint8_t readStatus()
+  {
+    return driver.readReg(HPSDRegAddr::STATUS);
+  }
+
+  /// Clears any status conditions that are currently latched in the driver.
+  ///
+  /// WARNING: Calling this function clears latched faults, which might allow
+  /// the motor driver outputs to reactivate.  If you do this repeatedly without
+  /// fixing an abnormal condition (like a short circuit), you might damage the
+  /// driver.  
+  void clearStatus()
+  {
+    driver.writeReg(HPSDRegAddr::STATUS, 0);
+  }
+  
+  /// Reads fault conditions indicated by the driver.
+  ///
+  /// The return value is the same as that which would be returned by
+  /// readStatus(), except it only contains bits that indicate faults (STATUS
+  /// bits 5:0).
+  uint8_t readFaults()
+  {
+    return readStatus() & 0b00111111;
+  }
+
+  /// Clears any fault conditions that are currently latched in the driver.
+  ///
+  /// This function behaves the same as clearStatus(), except it only clears
+  /// bits that indicate faults (STATUS bits 5:0).
+  ///
+  /// WARNING: Calling this function clears latched faults, which might allow
+  /// the motor driver outputs to reactivate.  If you do this repeatedly without
+  /// fixing an abnormal condition (like a short circuit), you might damage the
+  /// driver.   
+  void clearFaults()
+  {
+    driver.writeReg(HPSDRegAddr::STATUS, ~0b00111111);
+  }
+
+protected:
+
+    uint16_t ctrl, torque, off, blank, decay, stall, drive;
+
+    /// Writes the cached value of the CTRL register to the device.
+    void writeCTRL()
+    {
+      driver.writeReg(HPSDRegAddr::CTRL, ctrl);
+    }
+
+    /// Writes the cached value of the TORQUE register to the device.
+    void writeTORQUE()
+    {
+        driver.writeReg(HPSDRegAddr::TORQUE, torque);
+    }
+
+    /// Writes the cached value of the OFF register to the device.
+    void writeOFF()
+    {
+        driver.writeReg(HPSDRegAddr::OFF, off);
+    }
+
+    /// Writes the cached value of the BLANK register to the device.
+    void writeBLANK()
+    {
+        driver.writeReg(HPSDRegAddr::BLANK, blank);
+    }
+
+    /// Writes the cached value of the DECAY register to the device.
+    void writeDECAY()
+    {
+        driver.writeReg(HPSDRegAddr::DECAY, decay);
+    }
+
+    /// Writes the cached value of the STALL register to the device.
+    void writeSTALL()
+    {
+        driver.writeReg(HPSDRegAddr::STALL, stall);
+    }
+
+    /// Writes the cached value of the DRIVE register to the device.
+    void writeDRIVE()
+    {
+        driver.writeReg(HPSDRegAddr::DRIVE, drive);
+    }
+
+public:
+    /// This object handles all the communication with the DRV8711.  Generally,
+    /// you should not need to use it in your code for basic usage of a
+    /// High-Power Stepper Motor Driver, but you might want to use it to access
+    /// more advanced settings that the HighPowerStepperDriver class does not
+    /// provide functions for.
+    DRV8711SPI driver;
+};
--- examples/BasicStepping/BasicStepping.ino
+++ examples/BasicStepping/BasicStepping.ino
@@ -1,4 +1,4 @@
-/* This example shows basic use of the DRV8711 stepper motor driver.
+/* This example shows basic use of the DRV8711 sd motor driver.
 
    It shows how to initialize the driver, set the current limit, set 
    the micro-stepping mode, and enable the driver. It shows how to send 
@@ -11,96 +11,53 @@
  */
 
 #include <SPI.h>
-#include <DRV8711.h>
+#include <HighPowerStepperDriver.h>
 
-const uint8_t slaveSelectPin = 2;
-const uint8_t stepPin = 3;
-const uint8_t resetPin = 4;
-const uint8_t dirPin = 5;
-const uint8_t sleepPin = 6;
+const uint8_t chipSelectPin = 2;
 
-DRV8711 stepper;
+HighPowerStepperDriver sd;
 
 void setup() 
 {
   SPI.begin();
-  stepper.init(slaveSelectPin);
+  sd.setChipSelectPin(chipSelectPin);
   delay(1);
   
-  // Drive the RST, STEP, and DIR pins low initially. Drive the nSLP pin high initially.
-  digitalWrite(resetPin, LOW);
-  pinMode(resetPin, OUTPUT);
-  digitalWrite(stepPin, LOW);
-  pinMode(stepPin, OUTPUT);
-  digitalWrite(dirPin, LOW);
-  pinMode(dirPin, OUTPUT);
-  digitalWrite(sleepPin, HIGH);
-  pinMode(sleepPin, OUTPUT);
-
   // Reset the driver to it's default settings.
-  digitalWrite(resetPin, HIGH);
-  delay(1);
-  digitalWrite(resetPin, LOW);
-  delay(1);
-
+  sd.resetSettings();
+  
+  sd.setDecayMode(HPSDDecayMode::AutoMixed);
+  
   // Set the current limit. You should change the numbers here to
   // an appropriate value for your particular system.
   // The current limit is given by the equation below:
   // IFS = (2.75 * TORQUE)/(256 * ISGAIN * .03)
-  stepper.setTorque(56);
-  stepper.setGain(20);
+  sd.setCurrentLimit36v40(1000);
 
   // Set the step mode and enable the driver.
-  stepper.setStepMode(32);
-  stepper.enableDriver();
+  sd.setStepMode(HPSDStepMode::MicroStep32);
+  sd.enableDriver();
 }
 
 void loop() 
 {
   // Step in the default direction 1000 times.
-  setDirection(0);
+  sd.setDirection(0);
   for(unsigned int x = 0; x < 1000; x++)
   {
-    step();
+    sd.step();
   }
   
   // Wait for 300 ms.
   delay(300);
 
   // Step in the other direction 1000 times.
-  setDirection(1);
+  sd.setDirection(1);
   for(unsigned int x = 0; x < 1000; x++)
   {
-    step();
+    sd.step();
   }
 
   // Wait for 300 ms.
   delay(300);
-}
-
-// Sends a pulse on the STEP pin to tell the driver to take
-// one step, and also delays to control the speed of the motor.
-void step()
-{
-  // The NXT/STEP minimum high pulse width is 2 microseconds.
-  digitalWrite(stepPin, HIGH);
-  delayMicroseconds(3);
-  digitalWrite(stepPin, LOW);
-  delayMicroseconds(3);
-
-  // The delay here controls the stepper motor's speed.  You can
-  // increase the delay to make the stepper motor go slower.  If
-  // you decrease the delay, the stepper motor will go fast, but
-  // there is a limit to how fast it can go before it starts
-  // missing steps.
-  delayMicroseconds(2000);
-}
-
-void setDirection(bool dir)
-{
-  // The NXT/STEP pin must not change for at least 0.5
-  // microseconds before and after changing the DIR pin.
-  delayMicroseconds(1);
-  digitalWrite(dirPin, dir);
-  delayMicroseconds(1);
 }
